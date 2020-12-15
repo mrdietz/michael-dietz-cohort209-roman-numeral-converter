fa7b72b948b8efed2d1a7e3cc208f4ff92403862
mrdietz <mrdietz@gmail.com>
2020-12-15 01:09:18 -0800

    JS and DOM manip functional

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+function randomRoman() {
+    let famousRomans = ['Nero', 
+                        'Commodus', 
+                        'Tiberius Gracchus', 
+                        'Constantine', 
+                        'Cicero', 
+                        'Caesar', 
+                        'Spartacus', 
+                        'Augustus', 
+                        'Marcus Aurelius'];
+
+    let randomRoman = famousRomans[Math.floor(Math.random() * famousRomans.length)];
+    document.getElementById("randomRoman").innerHTML = randomRoman + ' would say...';
+};
+
 function convertToRoman(num) {
+    num = document.getElementById('userNum').value;
+
     var decValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
-
     var result = "";
 
     for (var i = 0; i < decValue.length; i++) {
@@ -9,7 +25,10 @@ function convertToRoman(num) {
             result += romanNum[i];
             num -= decValue[i];
         }
-    }
-
-  return result;
+    } 
+    document.getElementById("romanized").innerHTML = result;
 };
+
+
+
+
