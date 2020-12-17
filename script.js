db21d1ffda79a5a83084e681ff6306615ee8d7e0
mrdietz <mrdietz@gmail.com>
2020-12-17 00:46:43 -0800

    Add support for larger numbers. Revert to plain page for turn-in.

--- script.js
+++ script.js
@@ -18,8 +18,57 @@ function randomRoman() {
 function convertToRoman(num) {
     num = document.getElementById('userInput').value;
 
-    var decValue = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var decValue = [1000000, 
+                    900000, 
+                    500000, 
+                    400000, 
+                    100000, 
+                    90000, 
+                    50000, 
+                    40000, 
+                    10000, 
+                    9000, 
+                    5000, 
+                    4000, 
+                    1000, 
+                    900, 
+                    500, 
+                    400, 
+                    100, 
+                    90, 
+                    50, 
+                    40, 
+                    10, 
+                    9, 
+                    5, 
+                    4, 
+                    1];
+
+    var romanNum = ['M\u0305', 
+                    'C\u0305M\u0305', 
+                    'D\u0305', 
+                    'C\u0305D\u0305', 
+                    'C\u0305', 
+                    'X\u0305C\u0305', 
+                    'L\u0305', 
+                    'X\u0305L\u0305', 
+                    'X\u0305', 
+                    'I\u0305X\u0305', 
+                    'V\u0305', 
+                    'I\u0305V\u0305', 
+                    'M', 
+                    'CM', 
+                    'D', 
+                    'CD', 
+                    'C', 
+                    'XC', 
+                    'L', 
+                    'XL', 
+                    'X', 
+                    'IX', 
+                    'V', 
+                    'IV', 
+                    'I'];
     var result = "";
 
     for (var i = 0; i < decValue.length; i++) {
